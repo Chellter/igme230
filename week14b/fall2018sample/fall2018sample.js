437bb0ceb85ff84d4931bf43ec815db2bbf06917
chellter <mof2116@g.rit.edu>
2018-12-05 23:40:14 -0500

    updated practice practical

--- week14b/fall2018sample/fall2018sample.js
+++ week14b/fall2018sample/fall2018sample.js
@@ -1,1 +1,33 @@
 /* Don't use <script> tags in a linked js file! */
+
+$(".menuitem").click(function () {
+    $(this).next(".submenu").slideToggle("slow");
+});
+
+$("#close").click(function () {
+    $("#close").toggle();
+    $("#open").toggle();
+});
+
+$("#open").click(function () {
+    $("#close").toggle();
+    $("#open").toggle();
+});
+
+/*
+<h2>&lt;aside&gt; element</h2>
+<label for="content-menu">Select a file to load:</label>
+<select name="content-menu" id="choose-content">
+    <option value="content1.txt">Your Name</option>
+    <option value="content2.txt">Course Info</option>
+</select>
+*/
+
+let cont = ("content1.txt")  // sets default verse element
+    $("#choose-content").val(cont); // changes menu option to default
+    $("#content").load(cont);   // retrieves only default element
+
+    $("#choose-content").change(function() {
+        verse = $(this).val();
+        $("#content").load(verse); 
+    });
